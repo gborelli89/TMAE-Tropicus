12d028bef44cec01f36533c9778353f524a31235
Gabriel Borelli <gborelli_89@hotmail.com>
2022-05-01 13:27:00 -0300

    Pequenas correções na aula 7

--- aula7.jl
+++ aula7.jl
@@ -174,7 +174,7 @@ Logo
 
 $f_1 = f_{-1}$
 
-A matriz terá, portanto, uma linga adicional.
+A matriz terá, portanto, uma linha adicional.
 
 $\frac{-f_1 + 2f_0 - f_{-1}}{(Δx)^2} = \frac{-2f_1 + 2f_0}{(Δx)^2} =2$
 
@@ -193,7 +193,7 @@ $\frac{1}{(0,125)^2} \begin{bmatrix}
 				\end{bmatrix} \begin{bmatrix}
 						f_0 \\ f_1 \\ f_2 \\ \vdots \\ f_7 \\
 	  				\end{bmatrix} = \begin{bmatrix}
-	   					2 \\ 2 \\ 2 \\ \vdots \\ 2 \\
+	   					1 \\ 2 \\ 2 \\ \vdots \\ 2 \\
 		 			\end{bmatrix}$
 """
 
@@ -230,10 +230,10 @@ md"""
 
 Problema de oscilação de um sistema massa-mola de 1 GDL
 
-$m\frac{d^u}{dt^2} + ku =0$
+$m\frac{d^2u}{dt^2} + ku =0$
 
 **Condições de contorno** $\rightarrow$
-$u(0) = 1$ e $u''(0)=0$
+$u(0) = 1$ e $u'(0)=0$
 
 **Solução exata**
 
@@ -676,6 +676,12 @@ git-tree-sha1 = "f6250b16881adf048549549fba48b1161acdac8c"
 uuid = "c1c5ebd0-6772-5130-a774-d5fcae4a789d"
 version = "3.100.1+0"
 
+[[deps.LERC_jll]]
+deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
+git-tree-sha1 = "bf36f528eec6634efc60d7ec062008f171071434"
+uuid = "88015f11-f218-50d7-93a8-a6af411a945d"
+version = "3.0.0+1"
+
 [[deps.LZO_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
 git-tree-sha1 = "e5b909bcf985c5e2605737d2ce278ed791b89be6"
@@ -749,10 +755,10 @@ uuid = "4b2f31a3-9ecc-558c-b454-b3730dcb73e9"
 version = "2.35.0+0"
 
 [[deps.Libtiff_jll]]
-deps = ["Artifacts", "JLLWrappers", "JpegTurbo_jll", "Libdl", "Pkg", "Zlib_jll", "Zstd_jll"]
-git-tree-sha1 = "340e257aada13f95f98ee352d316c3bed37c8ab9"
+deps = ["Artifacts", "JLLWrappers", "JpegTurbo_jll", "LERC_jll", "Libdl", "Pkg", "Zlib_jll", "Zstd_jll"]
+git-tree-sha1 = "c9551dd26e31ab17b86cbd00c2ede019c08758eb"
 uuid = "89763e89-9b03-5906-acba-b20f662cd828"
-version = "4.3.0+0"
+version = "4.3.0+1"
 
 [[deps.Libuuid_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -897,9 +903,9 @@ uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
 
 [[deps.Qt5Base_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Fontconfig_jll", "Glib_jll", "JLLWrappers", "Libdl", "Libglvnd_jll", "OpenSSL_jll", "Pkg", "Xorg_libXext_jll", "Xorg_libxcb_jll", "Xorg_xcb_util_image_jll", "Xorg_xcb_util_keysyms_jll", "Xorg_xcb_util_renderutil_jll", "Xorg_xcb_util_wm_jll", "Zlib_jll", "xkbcommon_jll"]
-git-tree-sha1 = "ad368663a5e20dbb8d6dc2fddeefe4dae0781ae8"
+git-tree-sha1 = "c6c0f690d0cc7caddb74cef7aa847b824a16b256"
 uuid = "ea2cea3b-5b76-57ae-a6ef-0a8af62496e1"
-version = "5.15.3+0"
+version = "5.15.3+1"
 
 [[deps.REPL]]
 deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
